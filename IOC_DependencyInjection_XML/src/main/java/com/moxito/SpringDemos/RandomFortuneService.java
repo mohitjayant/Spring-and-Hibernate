08153893df34d832bc04e8c56c382a445203369a
mohitjayant <mohitj8790@gmail.com>
2020-08-20 21:23:03 +0530

    IOC and dependency injection demonstration

---
+++ IOC_DependencyInjection_XML/src/main/java/com/moxito/SpringDemos/RandomFortuneService.java
@@ -0,0 +1,19 @@
+package com.moxito.SpringDemos;
+
+import java.util.Random;
+
+public class RandomFortuneService implements FortuneService {
+
+	private String[] data = { "Beware of the wolf in sheep's clothing", "Diligence is the mother of good luck",
+			"The journey is the reward" };
+
+	private Random myRandom = new Random();
+
+	@Override
+	public String getFortune() {
+		int index = myRandom.nextInt(data.length);
+		String myFortune = data[index];
+		return myFortune;
+	}
+
+}
