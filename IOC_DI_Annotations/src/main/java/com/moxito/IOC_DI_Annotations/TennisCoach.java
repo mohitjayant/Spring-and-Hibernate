89310b9d895f0d78683b1705cddba061a07fdc1f
mohitjayant <mohitj8790@gmail.com>
2020-08-22 18:59:40 +0530

    configuring spring container using java annotation

--- IOC_DI_Annotations/src/main/java/com/moxito/IOC_DI_Annotations/TennisCoach.java
+++ IOC_DI_Annotations/src/main/java/com/moxito/IOC_DI_Annotations/TennisCoach.java
@@ -1,13 +1,58 @@
 package com.moxito.IOC_DI_Annotations;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
+	// field injection
+	@Autowired
+	@Qualifier("randomFortuneService")
+	private FortuneService fortuneService;
+
+	public TennisCoach() {
+		System.out.println(">>TennisCoach: inside default constructor");
+	}
+
+	// define my init method
+	@PostConstruct
+	public void doMyStartupStuff() {
+		System.out.println(">>TennisCoach: inside of doMyinitStuff()");
+
+	}
+
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">>TennisCoach: inside of doMyinitStuff()");
+	}
+
+	/*
+	 * @Autowired public TennisCoach(FortuneService fortuneService) {
+	 * this.fortuneService = fortuneService; }
+	 */
+
+	// Setter injection
+	/*
+	 * @Autowired public void setFortuneService(FortuneService fortuneService) {
+	 * System.out.println(">>TennisCoach: inside setFortuneService() method");
+	 * this.fortuneService = fortuneService; }
+	 */
+
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+
+		return fortuneService.getFortune();
 	}
 
 }
