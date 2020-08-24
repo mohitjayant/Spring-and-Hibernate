9ca1e8caf395155e83502d734ba0e6952f2b65b8
mohitjayant <mohitj8790@gmail.com>
2020-08-24 09:44:39 +0530

    Controller levelRequest Mapping

---
+++ SpringMVCDemo/src/main/java/com/moxito/mvcApp/SillyContoller.java
@@ -0,0 +1,13 @@
+package com.moxito.mvcApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SillyContoller {
+
+	@RequestMapping("/showForm")
+	public String displayTheForm() {
+		return "Silly";
+	}
+}
