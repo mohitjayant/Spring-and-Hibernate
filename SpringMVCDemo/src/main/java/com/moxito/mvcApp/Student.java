b5a574ff97fe45a0fb84d00f837e285bb4964fa3
mohitjayant <mohitj8790@gmail.com>
2020-08-26 12:59:17 +0530

    binding the list in drop-down list

--- SpringMVCDemo/src/main/java/com/moxito/mvcApp/Student.java
+++ SpringMVCDemo/src/main/java/com/moxito/mvcApp/Student.java
@@ -1,12 +1,27 @@
 package com.moxito.mvcApp;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
 	private String firstName;
 	private String lastName;
+	private String country;
+
+	private LinkedHashMap<String, String> countryOptions;
 
 	public Student() {
 
+		// populate country options
+		countryOptions = new LinkedHashMap<String, String>();
+
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+		countryOptions.put("JP", "Japan");
+		countryOptions.put("IS", "Israel");
+		countryOptions.put("US", "United State of America");
 	}
 
 	public String getFirstName() {
@@ -23,6 +38,18 @@ public class Student {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
 	}
 
 }
