4101a5916dc59dd19e263bf98d8fb1fd7888dcad
mohitjayant <mohitj8790@gmail.com>
2020-09-02 11:19:00 +0530

    validating Integer range

--- SpringMVCDemo/src/main/java/com/moxito/mvcApp/Customer.java
+++ SpringMVCDemo/src/main/java/com/moxito/mvcApp/Customer.java
@@ -1,15 +1,26 @@
 package com.moxito.mvcApp;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
 
 	private String firstName;
 
-	@NotNull
+	@NotNull(message = "is Required")
 	@Size(min = 1, message = "is Required")
 	private String lastName;
+
+	@NotNull(message = "is Required")
+	@Min(value = 0, message = "Value must be greater than equal to 0")
+	@Max(value = 10, message = "Value must be less than equal to 10")
+	private Integer freePasses;
+
+	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Only 5 chars/digits")
+	private String postalCode;
 
 	public String getFirstName() {
 		return firstName;
@@ -25,6 +36,22 @@ public class Customer {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public Integer getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(Integer freePasses) {
+		this.freePasses = freePasses;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 
 }
