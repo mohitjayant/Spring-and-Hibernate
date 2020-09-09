82f3f87cb76c3522af4b5db9598fc9c01c1b6b77
mohitjayant <mohitj8790@gmail.com>
2020-09-09 22:04:09 +0530

    testing connection

---
+++ One-To-One-Uni/src/main/java/com/moxito/jdbc/TestJdbc.java
@@ -0,0 +1,24 @@
+package com.moxito.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+
+		String jdbcUrlString = "jdbc:mysql://localhost:3306/one-to-one-uni?useSSL=false&serverTimezone=UTC";
+		String user = "moxito";
+		String pass = "moxito";
+
+		try {
+
+			System.out.println("Connecting to database: " + jdbcUrlString);
+			Connection myConnection = DriverManager.getConnection(jdbcUrlString, user, pass);
+			System.out.println("Connection Successful!!!");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
